df22191c28e687e098cc0960756132886cb259f0
Shubham Singh <shubhamsingh290596@gmail.com>
2022-12-05 22:17:03 +0530

    Middle of the Linked List

---
+++ a876.java
@@ -0,0 +1,26 @@
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+     ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode node =  head;
+        int n = 0;
+        while(node.next!=null){
+            node=node.next;
+            n++;
+        }
+        n=n/2;
+        while(n>0){
+            head=head.next;
+            n--;
+        }
+        System.out.println();
+        return head;
+    }
+}
